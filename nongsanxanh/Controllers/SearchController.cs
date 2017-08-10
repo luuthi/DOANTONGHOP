34833a549026f5b92bb98aa5fafa0a7fac632b73
luu dinh thi <luudinhthi22t.t@gmail.com>
2017-08-10 18:41:25 +0700

    last commit

---
+++ nongsanxanh/Controllers/SearchController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Bussiness.Interface;
+using Bussiness.ViewModel;
+using PagedList;
+
+namespace nongsanxanh.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IProductService _iProductService;
+        public SearchController(IProductService iProductService)
+        {
+            _iProductService = iProductService;
+        }
+        // GET: Search
+        public ActionResult Index(string searchString, int? page)
+        {
+            int pageSize = 10;
+            int pageNumber = (page ?? 1);
+            var strArr = searchString.Split(' ');
+            List<ProductViewModel> lst =new List<ProductViewModel>();
+            var rs = _iProductService.GetAllProduct();
+            foreach (string str in strArr)
+            {
+                var result=  rs.Where(m => m.ProductName.ToLower().Contains(str) && m.Status).ToList();
+                lst.AddRange(result);
+            }
+            var viewModel =  lst.GroupBy(m => m.Id).Select(o => o.FirstOrDefault()).ToList();
+            return View(viewModel.ToPagedList(pageNumber, pageSize));
+        }
+    }
+}
